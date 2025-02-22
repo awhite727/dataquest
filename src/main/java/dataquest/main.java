16f3c6632401fe53aaa52d8aeb29e3d4c76c1445
Mdodson06 <mariadodson8@gmail.com>
2025-02-21 18:41:49 -0600

    Turn to Maven project

--- src/main/java/dataquest/main.java
+++ src/main/java/dataquest/main.java
@@ -1,6 +1,9 @@
+package dataquest;
 
 public class main {
     public static void main(String [] args) {
         Graphics g = new Graphics();
-    }
+        Importing imp = new Importing();   
+        imp.gui();
+    }        
 }
